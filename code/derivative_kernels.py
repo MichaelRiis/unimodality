4eea53c1dc72afd6f02ba6fa9935fe82289cbce0
MichaelRiis <michael.riis@gmail.com>
2017-08-29 09:31:47 +0300

    Init commit of multidimensional case*

---
+++ code/derivative_kernels.py
@@ -0,0 +1,194 @@
+import numpy as np 
+import scipy as sc
+import pylab as plt
+import time
+
+
+# kernel functions
+cov_fun_base = lambda x, y, k1, k2: k1**2*np.exp(-0.5*np.sum((x[:, :, None]-y[None, :, :])**2, axis=1)/k2**2)
+cov_fun0 = lambda x, y, k1, k2: cov_fun_base(x, y, k1, k2)
+
+def cov_fun1(t2, t, g, k1, k2):
+    ### Implements cov[df^i/dx^i_g, f^j] ###
+
+    # C = np.zeros((len(t2), len(t)))
+
+    # for i in range(len(t2)):
+    #     for j in range(len(t)):
+    #         C[i,j] = cov_fun0(t2[i, None], t[j, None].T)*(-(t2[i, g]-t[j, g])/k2**2)
+
+    C = cov_fun_base(t2, t.T, k1, k2)*(-(t2[:, g, None] - t[:, g, None].T)/k2**2)
+
+    return C
+
+
+def cov_fun2(t2a, t2b, g, h, k1, k2):
+    ### Implements cov[df^i/dx^i_g, df^j/dx^j_h]
+
+    # C = np.zeros((len(t2a), len(t2b)))
+
+    # for i in range(len(t2a)):
+        # for j in range(len(t2b)):
+            # C[i,j] = cov_fun0(t2a[i, None], t2b[j, None].T) *(1.0*(g==h) - (t2a[i, h] - t2b[j, h])*(t2a[i, g] - t2b[j, g])/k2**2 )/k2**2
+
+    C =  cov_fun_base(t2a, t2b.T, k1, k2)*(1.0*(g==h) - (t2a[:, h, None] - t2b[:, h, None].T)*(t2a[:, g, None] - t2b[:, g, None].T)/k2**2 )/k2**2
+
+    return C
+
+
+
+def generate_joint_derivative_kernel(t, t_grad_list, k1, k2, jitter = 1e-8):
+    """ 
+        Generates SE kernel and its derivative kernels jointly
+
+        t:              list of input coordinates for the observation of the function
+        t_grad_list:    list with input coordinates for each partial derivative of f
+        k1:             magnitude parameters
+        k2:             lengthscale parameter
+
+    """
+
+    assert(t.ndim == 2), "Wrong dimensions for t"
+    for index, t_grad in enumerate(t_grad_list):
+        assert(t_grad.ndim == 2), "Wrong dimensions for t grad %d" % index
+        assert(t.shape[1] == t_grad.shape[1]), "Dimensions for inputs of the partial derivative %d (%d x %d) do not match the dimensions for the inputs of the regular observations (%d x %d)" % (index, t_grad.shape[0], t_grad.shape[1], t.shape[0], t.shape[1])
+
+        assert(t.shape[1] >= len(t_grad_list)), "Cant have more partial derivative than coordinates"
+
+    # Dimensions
+    N = len(t)
+    Ms = [len(t_grad) for t_grad in t_grad_list]
+    D = N + np.sum(Ms)
+
+
+
+
+
+    # Prepare for joint kernel
+    K = np.zeros((D, D))
+
+    # Kernel for regular observations (within)
+    K[:N, :N] = cov_fun0(t, t.T, k1, k2)
+
+    # Covariance between df/dx_g and f
+    offset = N
+    for g in range(len(Ms)):
+        K[offset:offset + Ms[g], :N] = cov_fun1(t_grad_list[g], t, g, k1, k2)
+        K[:N, offset:offset + Ms[g]] = K[offset:offset + Ms[g], :N].T
+        offset += Ms[g]
+
+    # Covariance between df/dx_g and df/dx_h
+    offset_g = N
+    for g in range(len(Ms)):
+        offset_h = N
+        for h in range(len(Ms)):
+            K[offset_g:offset_g + Ms[g], offset_h:offset_h + Ms[h]] = cov_fun2(t_grad_list[g], t_grad_list[h], g, h, k1, k2)
+            offset_h += Ms[h]
+
+        offset_g += Ms[g]
+
+
+    # jitter
+    K += jitter*np.identity(D)
+
+    return K
+
+
+if __name__ == "__main__":
+
+
+    np.random.seed(0)
+
+    # Kernel hyperparameters
+    k1, k2 = 2., 10.
+    
+    # Dimensions etc.
+    num_points_per_dim_f = 100
+    num_points_per_dim_fprime = 50
+    dim = 2
+    x_min, x_max = -8, 8
+
+    # Generate coordinates for function observations
+    coord_grids = np.meshgrid(*[np.linspace(x_min, x_max, num_points_per_dim_f) for i in range(dim)])
+    t = np.column_stack([grid.ravel() for grid in coord_grids])
+
+    # Generate coordinates for derivative observations
+    coord_grids = np.meshgrid(*[np.linspace(x_min, x_max, num_points_per_dim_fprime) for i in range(dim)])
+    tg = np.column_stack([grid.ravel() for grid in coord_grids])
+
+    # For each dim
+    t_grad_list = [tg.copy() for i in range(dim)]
+
+    # Create kernel
+    t0 = time.time()
+    print('Construction kernel...', end='')
+    K = generate_joint_derivative_kernel(t, t_grad_list, k1, k2)
+    assert(np.allclose(K, K.T)), "Matrix must be symmetric!"
+    t1 = time.time()
+    td = t1 - t0
+    print('done in %4.3fs' % td)
+
+
+    try:
+
+      t0 = time.time()
+      print('Computing Cholesky decomposition...', end='')
+      L = np.linalg.cholesky(K)
+      t1 = time.time()
+      td = t1 - t0
+      print('done in %4.3fs' % td)
+
+      if dim == 2:
+
+        # sample f, fprime_1, fprime_2 jointly
+        t0 = time.time()
+        print('Sampling...', end='')
+        sample = np.dot(L, np.random.normal(0, 1, size = len(K)))
+        t1 = time.time()
+        td = t1 - t0
+        print('done in %4.3fs' % td)
+
+        # split into parts
+        f = sample[:num_points_per_dim_f**2].reshape((num_points_per_dim_f, num_points_per_dim_f))
+        fp1 = sample[num_points_per_dim_f**2:num_points_per_dim_f**2 + num_points_per_dim_fprime**2].reshape((num_points_per_dim_fprime, num_points_per_dim_fprime))
+        fp2 = sample[-num_points_per_dim_fprime**2:].reshape((num_points_per_dim_fprime, num_points_per_dim_fprime))
+
+
+        # compute finite difference approximation of the gradients
+        fp1_approx, fp2_approx = np.zeros_like(f), np.zeros_like(f)
+        D1, D2 = fp1_approx.shape
+        d = (x_max - x_min)/num_points_per_dim_f
+
+        for i in range(fp1_approx.shape[0]):
+            for j in range(fp1_approx.shape[1]):
+
+                n, m = np.min((D1-1, i+1)), np.max((0, i-1))
+                fp2_approx[i,j] = (f[n, j] - f[m, j])/(2*d)
+
+                n, m = np.min((D2-1, j+1)), np.max((0, j-1))
+                fp1_approx[i,j] = (f[i, n] - f[i, m])/(2*d)
+        # plot
+        plt.figure()
+
+        xs = np.linspace(x_min, x_max, num_points_per_dim_f)
+        xp = np.linspace(x_min, x_max, num_points_per_dim_fprime)
+
+
+        for index, (xi, fi) in enumerate([(xs, f), (xp, fp1), (xp, fp2), (None, None), (xs, fp1_approx), (xs, fp2_approx)]):
+
+            if fi is None:
+                continue
+
+            plt.subplot(2, 3, 1 + index)
+            plt.pcolormesh(xi, xi, fi, cmap=plt.cm.RdBu_r)
+            plt.grid(False)
+            plt.colorbar()
+            plt.axis('equal')
+            plt.axis('off')
+
+    except Exception as e:
+      print('Faaaaiiiiil! Something went wrong')
+      print(e)
+
+
+    plt.show()
---
+++ code/test_2d.py
@@ -0,0 +1,170 @@
+import numpy as np
+import pylab as plt
+
+from scipy.stats import norm
+
+import GPy
+
+from util import plot_with_uncertainty
+import ep_unimodality_2d as ep
+from importlib import reload
+reload(ep)
+
+phi = lambda x: norm.cdf(x)
+
+# set seed
+np.random.seed(0)
+
+# Define test function
+f = lambda x, y: 0.1*(x**2 + y**2 + -1.2*x*y)
+
+
+# For prediction and plotting
+xs = np.linspace(-10, 10, 41)
+ys = np.linspace(-10, 10, 41)
+Xp, Yp = np.meshgrid(xs, ys)
+XY = np.column_stack((Xp.ravel(), Yp.ravel()))
+
+# sample points
+sigma2 = 1.
+N = 25
+X = np.random.normal(0, 3, size = (N, 2))
+y = f(X[:, 0], X[:, 1])[:, None] + np.random.normal(0, np.sqrt(sigma2), size=(N, 1))
+
+# generate test set
+Ntest = 1000
+Xtest = np.random.normal(0, 2.5, size = (Ntest, 2))
+ytest = f(Xtest[:, 0], Xtest[:, 1])[:, None] + np.random.normal(0, np.sqrt(sigma2), size=(Ntest, 1))
+
+# GP hyperparameters
+variance = 10
+lengthscale = 4
+
+# Fit regular GP
+kernel = GPy.kern.RBF(input_dim=2, lengthscale=lengthscale, variance=variance)
+if N > 0:
+	model = GPy.models.GPRegression(X=X, Y=y, kernel=kernel, noise_var=sigma2)
+	# model.optimize()
+	mu, var = model.predict_noiseless(XY)
+	mu = mu.reshape((len(xs), len(ys)))
+	var = var.reshape((len(xs), len(ys)))
+
+	gpy_lppd = np.mean(model.log_predictive_density(Xtest, ytest))
+else:
+	mu = np.zeros((len(xs), len(ys)))
+	var = variance*np.ones((len(xs), len(ys)))
+
+
+# Fit Unimodal GP
+M = 10
+x1 = np.linspace(-10, 10, M)
+x2 = np.linspace(-10, 10, M)
+X1, X2 = np.meshgrid(x1, x2)
+Xd = np.column_stack((X1.ravel(), X2.ravel()))
+
+
+mu_f, Sigma_f, Sigma_full_f, g_posterior_list, Kf = ep.ep_unimodality(X, y, k1=np.sqrt(variance), k2=lengthscale, sigma2=sigma2, t2=Xd, verbose=10, nu2=1.)
+mu_ep, var_ep = ep.predict(mu_f, Sigma_full_f, X, Xd, XY, k1=np.sqrt(variance), k2=lengthscale, sigma2=sigma2)
+mu_ep_g1, var_ep_g1 = ep.predict(g_posterior_list[0][0], g_posterior_list[0][2], Xd, Xd, XY, k1=np.sqrt(variance), k2=lengthscale, sigma2=sigma2)
+mu_ep_g2, var_ep_g2 = ep.predict(g_posterior_list[1][0], g_posterior_list[0][2], Xd, Xd, XY, k1=np.sqrt(variance), k2=lengthscale, sigma2=sigma2)
+
+mu_ep = mu_ep.reshape((len(xs), len(ys)))
+var_ep = var_ep.reshape((len(xs), len(ys)))
+mu_ep_g1 = mu_ep_g1.reshape((len(xs), len(ys)))
+mu_ep_g2 = mu_ep_g2.reshape((len(xs), len(ys)))
+
+var_ep_g1 = var_ep_g1.reshape((len(xs), len(ys)))
+var_ep_g2 = var_ep_g2.reshape((len(xs), len(ys)))
+
+unimodal_lppd = ep.lppd(ytest, mu_f, Sigma_full_f, X, Xd, Xtest, k1=np.sqrt(variance), k2=lengthscale, sigma2=sigma2)
+
+
+
+# for line plot
+if N > 0:
+	idx = np.argmin(np.sum(X**2, axis = 1))
+	x0, y0 = X[idx, 0], X[idx, 1]
+	idx2 = np.argmin((xs-x0)**2)
+else:
+	x0, y0 = 0, 0
+	idx2 = np.argmin((xs-x0)**2)
+
+
+print('GPY LPPD: %4.3f' % gpy_lppd)
+print('Uni LPPD: %4.3f' % unimodal_lppd)
+
+# plot
+fig = plt.figure(figsize=(10, 6))
+plt.subplot(2, 3, 1)
+plt.contourf(xs, ys, f(Xp, Yp), 30)
+plt.axhline(y0, color='k', linestyle='--')
+plt.plot(X[:, 0], X[:, 1], 'r.')
+plt.colorbar()
+plt.title('True functions and observations')
+
+plt.subplot(2, 3, 2)
+plt.contourf(xs, ys, mu, 30)
+plt.plot(X[:, 0], X[:, 1], 'r.')
+plt.colorbar()
+plt.title('Regular GP')
+
+plt.subplot(2, 3, 3)
+plt.contourf(xs, ys, mu_ep, 30)
+plt.plot(X[:, 0], X[:, 1], 'r.')
+plt.colorbar()
+plt.title('Unimodal GP')
+
+if M > 0:
+
+	plt.subplot(2, 3, 5)
+	mu_g = g_posterior_list[0][0][:(len(x1)*len(x2))].reshape((len(x1), len(x2)))
+	plt.contourf(xs, xs, phi(mu_ep_g1/np.sqrt(1+var_ep_g1)), 30)
+	plt.plot(Xd[:, 0], Xd[:, 1], 'k.', markersize = 2, alpha=0.75)
+	plt.colorbar()
+	plt.title('Posterior mean deriv1')
+
+	plt.subplot(2, 3, 6)
+	# mu_g = g_posterior_list[1][0][:(len(x1)*len(x2))].reshape((len(x1), len(x2)))
+	plt.contourf(xs, ys, phi(mu_ep_g2/np.sqrt(1+var_ep_g2)), 30)
+	plt.plot(Xd[:, 0], Xd[:, 1], 'k.', markersize = 2, alpha=0.75)
+	plt.colorbar()
+	plt.title('Posterior mean deriv2')
+
+
+plt.figure()
+plt.subplot(1, 2, 1)
+mu_g = g_posterior_list[0][0]
+mu_g = mu_g[(M**2):2*(M**2)]
+mu_g = mu_g.reshape((M, M))
+
+plt.contourf(x1, x2, mu_g, 30)
+plt.colorbar()
+
+plt.subplot(1, 2, 2)
+mu_g = g_posterior_list[1][0]
+mu_g = mu_g[(M**2):2*(M**2)]
+mu_g = mu_g.reshape((M, M))
+
+plt.contourf(x1, x2, mu_g, 30)
+plt.colorbar()
+
+fig.tight_layout()
+
+
+# fit line
+plt.figure()
+plt.plot(xs, f(xs, y0), label = 'True function', color='g')
+# plt.plot(x0, f(x0, y0), 'k.')
+plot_with_uncertainty(xs,  mu_ep[idx2, :], ystd=np.sqrt(var_ep[idx2, :]), color='r', label='Unimodal', linestyle='--')
+plot_with_uncertainty(xs,  mu[idx2, :], ystd=np.sqrt(var[idx2, :]), color='b', label='Vanilla')
+plt.legend()
+plt.grid(True)
+plt.show(block=False)
+
+
+plt.show()
+
+
+
+
+

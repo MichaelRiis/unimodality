c283bfaf14e99ace0426d3388d6973a6b8fa32ca
MichaelRiis <michael.riis@gmail.com>
2017-09-28 14:46:56 +0300

    Add batch scripts for 1D experiments

---
+++ experiments/generate_batch_script_1d.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+output_filebase = "run_1d_batch"
+
+base_str = "python run_1d.py"
+function_classes = ['gaussian', 'student_t']
+max_functions = 20
+
+lines_per_job = 20
+time_per_line = 10
+
+# generate run line for each run
+lines = []
+for function_class in function_classes:
+	for idx_fun in range(max_functions):
+		lines.append(base_str + " --function_class " + function_class + " --function_idx %d" % idx_fun )
+
+
+# computer number of jobs and time per job
+num_lines = len(lines)
+num_jobs = np.ceil(num_lines/lines_per_job).astype(int)
+hours_per_job = np.ceil(time_per_line*lines_per_job/60).astype(int)
+assert(hours_per_job < 24)
+
+
+print('%d jobs needed with a maximum run time of %4.3f hour(s)' % (num_jobs, hours_per_job))
+
+# split in job files
+
+for idx_job in range(num_jobs):
+
+	output_file = output_filebase +  "%d"%idx_job + '.sh'
+
+	print(100*'-')
+	print('Job %d: filename = %s ' % (idx_job, output_file))
+	print(100*'-')
+
+	f  = open(output_file, 'w') 
+
+
+	f.write('#!/bin/bash\n')
+	f.write('#SBATCH --time=0-00:%02d:00\n' % hours_per_job)
+	f.write('#SBATCH --mem-per-cpu=1000\n')
+	f.write('\n')
+
+	# move first part of 'lines' to 'current_lines'
+	current_lines = [lines.pop(0) for idx_line in range(lines_per_job)]
+
+	for line in current_lines:
+		f.write(line + '\n')
+
+	f.close()
+
+	print('Done.')
+	print('\n\n')
+
+
+
+
+
---
+++ experiments/plot_results_1d.py
@@ -0,0 +1,140 @@
+import numpy as np
+import pylab as plt
+import seaborn as snb
+import sys
+import argparse
+import re
+import os
+
+from collections import OrderedDict
+from os import listdir
+from os.path import isfile, join
+
+sys.path.append('../code/')
+from util import plot_with_uncertainty
+
+
+#############################################################################################################
+# Read all files in directory
+#############################################################################################################
+def read_from_directory(directory):
+	all_files = [f for f in listdir(directory) if isfile(join(directory, f))]
+	print('Found %d files in total in %s' % (len(all_files), directory))
+
+	# extract ids
+	ids = np.array([], dtype=int)
+
+	files = []
+	for f in all_files:
+		
+		numbers_list = list(map(int, re.findall('\d+', f)))
+
+		# Check that we have two and only two number
+		if(len(numbers_list) != 3):
+			print('Found %d numbers in filename %s, skipping...' % (len(numbers_list), f))
+			continue
+
+		# store
+		ids = np.append(ids, numbers_list[1])
+		files.append(f)
+
+	num_files = len(files)
+	print('Found %d valid files in total in %s' % (num_files, directory))
+
+
+	# load data
+	results_X = {name: [] for name in models}
+	results_Y = {name: [] for name in models}
+
+	for f, idx in zip(files, ids):
+
+		raw_data = np.load(join(directory, f))
+		for name in models:
+			results_X[name].append(raw_data['results_X'][()][name])		
+			results_Y[name].append(raw_data['results_Y'][()][name])	
+
+	return results_X, results_Y
+
+#############################################################################################################
+# Metrics as a function of iterations
+#############################################################################################################
+def function_value(X, Y):
+	return X, Y
+
+
+def best_value(X, Y):
+	return X, np.minimum.accumulate(np.stack(Y), axis=1)
+
+
+metrics = OrderedDict([ ('Function value', function_value),
+                        ('Best value', best_value)])
+
+
+#############################################################################################################
+# Parse arguments
+#############################################################################################################
+parser = argparse.ArgumentParser()
+parser.add_argument('--directory_base', help='results directory', default = 'results')
+parser.add_argument('--dim', type=int, help='dimension', default=1)
+
+# extract
+args = parser.parse_args()
+directory_base = args.directory_base
+dim = args.dim
+
+
+
+models = ['regular', 'unimodal', 'unimodal2']
+function_classes = ['gaussian', 'student_t']
+
+
+
+#############################################################################################################
+# Plot
+#############################################################################################################
+colors = snb.color_palette()
+
+plt.figure()
+
+for idx_class, function_class in enumerate(function_classes):
+
+	# read data
+	directory = '{}_{}_{}d'.format(directory_base, function_class, dim)
+	results_X, results_Y = read_from_directory(directory)
+
+	# for each metric	
+	for idx_metric, metric_name in enumerate(metrics):
+		plt.subplot2grid((len(function_classes), len(metrics)), (idx_class, idx_metric))
+
+		metric_fun = metrics[metric_name]
+		
+		for name, color in zip(models, colors):
+
+			X, Y = metric_fun(results_X[name], results_Y[name])
+
+			Y_mean, Y_var = np.mean(Y, axis=0), np.var(Y, axis=0)/len(X)
+
+			plot_with_uncertainty(np.arange(1, len(Y_mean) + 1), Y_mean, yvar=Y_var, color=color, label=name)
+
+		plt.legend()
+		plt.grid(True)
+		plt.xlabel('Number of iterations')
+		plt.ylabel(metric_name)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

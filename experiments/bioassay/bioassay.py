a4ec7b503828db8d1db069059b12abd116f4d84c
MichaelRiis <michael.riis@gmail.com>
2017-10-19 15:49:30 +0300

    Add functions for fitting with gauss mean function

--- experiments/bioassay/bioassay.py
+++ experiments/bioassay/bioassay.py
@@ -1,11 +1,16 @@
-import numpy as np
+import autograd.numpy as np
 import pylab as plt
 import seaborn as snb
 import time
 import argparse
 import GPy
+from GPy.core.parameterization import Param
+from GPy.util import choleskies
+
 
 from scipy.stats import multivariate_normal as mvn
+from scipy.optimize import minimize
+from autograd import value_and_grad, grad, hessian
 
 import sys
 sys.path.append('../../code')
@@ -22,8 +27,68 @@ n = np.array([5, 5, 5, 5])
 y = np.array([0, 1, 3, 5])
 
 # for plotting
-A = np.linspace(-4, 8, 100)
-B = np.linspace(-10, 40, 100)
+A = np.linspace(-4, 8, 200)
+B = np.linspace(-10, 40, 200)
+
+#############################################################################################
+# Functions for using log gauss density as mean function
+#############################################################################################
+def log_mvn_chol(x, mu, L):
+
+    D = x.shape[1]
+
+    logdet = 2*np.sum(np.log(np.diag(L)))
+    det = np.exp(logdet)
+    v = x - mu
+    b = np.linalg.solve(L, v.T).T
+
+    return -0.5*D*np.log(2*np.pi) - 0.5*logdet -0.5*np.sum(b**2, axis=1, keepdims=True)
+
+class LogGaussMap(GPy.core.Mapping):
+
+    def __init__(self, input_dim, output_dim, name='gaussmap', mean=None, cov=None):
+        super(LogGaussMap, self).__init__(input_dim=input_dim, output_dim=output_dim, name=name)
+        
+        self.mean = Param('mean', np.zeros((input_dim, 1)))
+        if not mean is None:
+            self.mean = mean
+            
+        # initial chol
+        if cov is None:
+            C = np.identity(input_dim)
+        else:
+            C = np.linalg.cholesky(cov)
+
+        chols = choleskies.triang_to_flat(np.tile(C[None,:, :], (1,1,1)))
+        self.chols = Param('chols', chols)
+
+    def f(self, X):
+        L = choleskies.flat_to_triang(self.chols)[0]
+        log_pdf = log_mvn_chol(X, self.mean.ravel(), L)
+        return log_pdf
+        
+
+    def update_gradients(self, dL_dF, X):
+        pass
+
+    def gradients_X(self, dL_dF, X):
+        return None
+
+
+
+def compute_laplace():
+    obj = lambda x: -log_posterior(x[0], x[1])
+
+    res = minimize(value_and_grad(obj), np.array((0.3, 10.)), jac=True)
+    mode = res.x
+    hess = hessian(obj)(mode)
+    S = np.linalg.inv(hess)
+
+    print('Found mode\t(a,b) = %r' % mode)
+    print('Found covariance\t (a,b) = %r' % S)
+
+    return mode, S
+
 
 
 #############################################################################################
@@ -75,6 +140,22 @@ def fit_regular(X, Y):
 	reg_gp.optimize()
 
 	return reg_gp
+
+@timeit
+def fit_regular_gauss(X, Y):
+
+	# get mean and variance of mean function from Laplace approximation
+	mode, S = compute_laplace()
+
+	# setup mean funciton
+	mf = LogGaussMap(input_dim=2, output_dim=1, mean=mode[:, None], cov=S)
+
+	gauss_gp = GPy.models.GPRegression(X=X, Y=Y, kernel=rbf.copy() + bias.copy(), noise_var=1e-3, mean_function=mf)
+	gauss_gp.likelihood.variance.constrain_fixed()
+	gauss_gp.optimize()
+
+	return gauss_gp
+
 
 @timeit
 def predict_grid(model, A, B):
@@ -142,10 +223,12 @@ def log_likelihood(a,b,x,y,n):
 
 	# negative log posterior (error function to be minimized)
 	lp = np.sum( y*t  -n*np.log1p(et) )
+
 	return lp 
 
 def log_prior(a, b):
 	return 0
+
 
 def log_posterior(a, b):
 	return log_prior(a, b) + log_likelihood(a, b, x, y, n)

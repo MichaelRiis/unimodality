4df3dd71aff992df3cb860c9c83d6009ec70b309
MichaelRiis <michael.riis@gmail.com>
2017-10-20 17:28:59 +0300

    Change number of points

--- experiments/bioassay/generate_batch_maxvar.py
+++ experiments/bioassay/generate_batch_maxvar.py
@@ -12,7 +12,7 @@ function_classes = test_function_base.test_function_dict.keys()
 
 max_seeds = 50
 lines_per_job = 5
-time_per_line = 30
+time_per_line = 40
 
 # generate run line for each run
 lines = []
